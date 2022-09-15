5ee90ac7f46eae6fddac5c4ec5bf81988b15e084
benazirgitacc <bencraft786@gmail.com>
2022-09-15 16:35:14 +0530

    onetest

---
+++ src/org/test/EmployeeDetails.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class EmployeeDetails {
+ public void empName() {
+	System.out.println("Benazir");
+}
+ public void empMob() {
+	System.out.println("8610370282");
+
+}
+ public static void main(String[] args) {
+	EmployeeDetails e=new EmployeeDetails();
+	e.empName();
+	e.empMob();
+}
+ 
+}
